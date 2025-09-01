053f579d20cb53952e234c6c9df79921975bac0b
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-09-01 13:02:04 -0300

    atualização

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Cards from "./components/Cards";
+
+export default () => {
+
+  return (
+    <div>
+      <Cards />
+    </div>
+  )
+}
---
+++ frontend/src/components/Cards.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => {
+
+    return (
+        <div className="size-10 bg-red-100">
+
+        </div>
+    )
+}
